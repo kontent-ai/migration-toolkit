9e6923e45d6e4c0bbbefcff037629ae59b502813
Richard Sustek <ennspassion@gmail.com>
2026-02-16 13:20:56 +0100

    fixes test scripts

--- lib/metadata.ts
+++ lib/metadata.ts
@@ -1,5 +1,5 @@
 export const libMetadata = {
 	name: "@kontent-ai/migration-toolkit",
-	timestamp: "Mon, 16 Feb 2026 12:02:49 GMT",
+	timestamp: "Mon, 16 Feb 2026 12:07:06 GMT",
 	version: "3.0.1",
 };
