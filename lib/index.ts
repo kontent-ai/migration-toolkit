507238d8a82468b308805c28db0c49658e18ea0a
Richard Sustek <ennspassion@gmail.com>
2026-03-25 10:37:59 +0100

    feat: export migration schemas

--- lib/index.ts
+++ lib/index.ts
@@ -3,6 +3,7 @@ export { getDefaultLogger } from "./core/logs/loggers.js";
 export { MigrationToolkitError, MissingAssetError, MissingItemError } from "./core/models/error.models.js";
 export type { Logger, LogMessage, LogSpinnerData, LogSpinnerMessage } from "./core/models/log.models.js";
 export * from "./core/models/migration.models.js";
+export * from "./core/models/migration.schema.js";
 export { defaultExternalIdGenerator, type ExternalIdGenerator } from "./core/utils/external-id.utils.js";
 export type { SourceExportItem } from "./export/export.models.js";
 export { elementsBuilder } from "./toolkit/elements-builder.js";
